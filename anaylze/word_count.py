81659d7b666d061d7b224d87f9bb3a7f146d25a6
goto <gotocoto901@gmail.com>
2024-09-01 00:55:03 -0400

    Working with only the 5000 must used words for now

--- anaylze/word_count.py
+++ anaylze/word_count.py
@@ -1,19 +1,16 @@
 import json
-import pandas as pd
-from collections import defaultdict, Counter
-from sqlalchemy import create_engine, text
 import re
 import os
-import numpy as np
 import logging
 import time
-import matplotlib.pyplot as plt
+from collections import Counter
+from sqlalchemy import create_engine, text
 import mysql.connector
 import csv
+start_time = time.time()
 # Load database credentials
 with open('db_config.json') as f:
     db_config = json.load(f)
-
 # Connect to the database
 db_url = f"mysql+pymysql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
 engine = create_engine(db_url)
@@ -25,7 +22,7 @@ def clean_comment(text):
     return clean_re.sub(' ', text).lower()
 
 # List of trivial words (stop words)
-def load_words(file_path):
+def load_stop_words(file_path):
     stop_words = set()
     try:
         with open(file_path, 'r') as file:
@@ -39,35 +36,20 @@ def load_words(file_path):
 
 # Path to the stop words file
 stop_words_file_path = os.path.join(os.path.dirname(__file__), 'stop_words.txt')
-approved_words_file_path = os.path.join(os.path.dirname(__file__), 'stop_words.txt')
+allowed_words_file_path = os.path.join(os.path.dirname(__file__), 'allowed_words.txt')
 # Load stop words from the file into a set
-stop_words = load_words(stop_words_file_path)
-aproved_words = load_words(approved_words_file_path)
+stop_words = load_stop_words(stop_words_file_path)
+allowed_words = load_stop_words(allowed_words_file_path)
 def filter_stop_words(words):
-    return [word for word in words if word in aproved_words]
+    return [word for word in words if word not in stop_words and len(word) <= 40]
 
 # Create the data folder if it doesn't exist
 data_folder = 'data'
 if not os.path.exists(data_folder):
     os.makedirs(data_folder)
 
-# Paths for saving files
-matrix_save_path = os.path.join(data_folder, 'word_matrix.npy')
-word_to_index_save_path = os.path.join(data_folder, 'word_to_index.json')
-count_file = os.path.join(data_folder, 'count.txt')
-
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-def load_count(count_file):
-    if os.path.exists(count_file):
-        with open(count_file, 'r') as f:
-            return int(f.read().strip())
-    return 0
-
-def save_count(count, count_file):
-    with open(count_file, 'w') as f:
-        f.write(str(count))
 
 def load_last_seen_id(last_seen_id_file):
     try:
@@ -84,28 +66,15 @@ def save_last_seen_id(last_seen_id, last_seen_id_file):
     except Exception as e:
         logging.error(f"Error saving last seen ID file: {e}")
 
-def save_plot(comments_processed, word_pairs, save_path='word_pairs_plot.png'):
-    plt.figure(figsize=(10, 6))
-    plt.plot(comments_processed, word_pairs, marker='o')
-    plt.title('Number of Word Pairs vs. Number of Comments Processed')
-    plt.xlabel('Number of Comments Processed')
-    plt.ylabel('Number of Word Pairs')
-    plt.grid(True)
-    plt.savefig(save_path)
-    plt.close()
-
 def process_batches(batch_size=100000, save_interval=5):
-    last_seen_id_file = 'last_seen_id.json'  # File to store the last seen ID
+    last_seen_id_file = 'last_seen_id.json'
     last_seen_id = load_last_seen_id(last_seen_id_file)
     
     # Initialize variables
-    word_matrix = defaultdict(Counter)
+    word_counter = Counter()
     total_processed = 0
-    start_time = time.time()
     batch_counter = 0
-    comments_processed = []
-    word_pairs_list = []
-    
+    csv_file_path = '/var/lib/mysql-files/word_pairs.csv'
     try:
         with engine.connect() as connection:
             last_seen_id = "0"
@@ -133,101 +102,107 @@ def process_batches(batch_size=100000, save_interval=5):
                     comment = clean_comment(row[1])
                     words = comment.split()
                     filtered_words = filter_stop_words(words)
-                    unique_words = set(filtered_words)
-                    
-                    for word1 in unique_words:
-                        for word2 in unique_words:
-                            word_matrix[word1][word2] += 1
+                    word_counter.update(filtered_words)
                 
                 total_processed += len(batch)
-                comments_processed.append(total_processed)
-                word_pairs_list.append(sum(len(word_matrix[word1]) for word1 in word_matrix))
-                
                 batch_counter += 1
-                #word_pairs = [(word1, word2, count) for word1 in word_matrix for word2, count in word_matrix[word1].items()]
+                
                 if batch_counter >= save_interval:
-                    # Prepare data for CSV
-                    insert_data = [{'word1': word1, 'word2': word2, 'count': count} for word1 in word_matrix for word2, count in word_matrix[word1].items()]
+                    # Save word counts to the database
+                    insert_data = [{'word': word, 'count': count} for word, count in word_counter.items()]
                     
-                    # File path for the CSV
-                    csv_file_path = '/var/lib/mysql-files/word_pairs.csv'
-                    
-                    # Save data to CSV
-                    with open(csv_file_path, mode='w', newline='', encoding='utf-8') as file:
-                        writer = csv.writer(file)
-                        writer.writerow(['word1', 'word2', 'count'])  # Write header
-                        for row in insert_data:
-                            writer.writerow([row['word1'], row['word2'], row['count']])
-                    
-                    # Load data from CSV into the database
                     # Define SQL queries
                     create_temp_table_query = """
-                        CREATE TEMPORARY TABLE temp_word_pairs (
-                            word1 VARCHAR(255),
-                            word2 VARCHAR(255),
+                        CREATE TEMPORARY TABLE temp_word_count (
+                            word VARCHAR(40),
                             count INT
                         );
                     """
-
+                    
                     load_into_temp_query = f"""
                         LOAD DATA INFILE '{csv_file_path}'
-                        INTO TABLE temp_word_pairs
+                        INTO TABLE temp_word_count
                         FIELDS TERMINATED BY ','
                         LINES TERMINATED BY '\\n'
                         IGNORE 1 LINES
-                        (word1, word2, count);
+                        (word, count);
                     """
 
                     merge_data_query = """
-                        INSERT INTO word_pairs (word1, word2, count)
-                        SELECT word1, word2, SUM(count)
-                        FROM temp_word_pairs
-                        GROUP BY word1, word2
+                        INSERT INTO word_count (word, count)
+                        SELECT word, SUM(count)
+                        FROM temp_word_count
+                        GROUP BY word
                         ON DUPLICATE KEY UPDATE
                             count = count + VALUES(count);
                     """
 
                     drop_temp_table_query = """
-                        DROP TEMPORARY TABLE temp_word_pairs;
+                        DROP TEMPORARY TABLE temp_word_count;
                     """
 
                     try:
-                            connection.execute(text(create_temp_table_query))
-                            connection.execute(text(load_into_temp_query))
-                            connection.execute(text(merge_data_query))
-                            connection.execute(text(drop_temp_table_query))
-                            connection.commit()  
-                            logging.info(f"Inserted {len(insert_data)} pairs")
+                        # Save data to a CSV file
+                        
+                        with open(csv_file_path, mode='w', newline='', encoding='utf-8') as file:
+                            writer = csv.writer(file)
+                            writer.writerow(['word', 'count'])  # Write header
+                            for row in insert_data:
+                                writer.writerow([row['word'], row['count']])
+                        
+                        connection.execute(text(create_temp_table_query))
+                        connection.execute(text(load_into_temp_query))
+                        connection.execute(text(merge_data_query))
+                        connection.execute(text(drop_temp_table_query))
+                        connection.commit()
+                        logging.info(f"Inserted {len(insert_data)} word counts")
                     except Exception as e:
-                        print(f"An error occurred while loading data: {e}")
-                        connection.rollback()  # Rollback on error
+                        logging.error(f"An error occurred while loading data: {e}")
+                        connection.rollback()
                     
                     save_last_seen_id(last_seen_id, last_seen_id_file)
 
-                
-
                     # Reset for the next batch
-                    word_matrix.clear()
+                    word_counter.clear()
                     batch_counter = 0
 
-                    # Save plot
-                    save_plot(comments_processed, word_pairs_list)
-                    
                 batch_end_time = time.time()
                 logging.info(f"Processed {total_processed} comments so far.")
                 logging.info(f"Batch processing time: {batch_end_time - batch_start_time:.2f} seconds.")
 
             # Final save if needed
-            if word_matrix:
-                save_plot(comments_processed, word_pairs_list)
+            if word_counter:
+                # Save remaining word counts to the database
+                insert_data = [{'word': word, 'count': count} for word, count in word_counter.items()]
+                
+                csv_file_path = '/var/lib/mysql-files/word_pairs.csv'
+                with open(csv_file_path, mode='w', newline='', encoding='utf-8') as file:
+                    writer = csv.writer(file)
+                    writer.writerow(['word', 'count'])
+                    for row in insert_data:
+                        writer.writerow([row['word'], row['count']])
+                
+                try:
+                    connection.execute(text(create_temp_table_query))
+                    connection.execute(text(load_into_temp_query))
+                    connection.execute(text(merge_data_query))
+                    connection.execute(text(drop_temp_table_query))
+                    connection.commit()
+                    logging.info(f"Inserted {len(insert_data)} word counts")
+                except Exception as e:
+                    logging.error(f"An error occurred while loading data: {e}")
+                    connection.rollback()
+                
+                save_last_seen_id(last_seen_id, last_seen_id_file)
 
     except KeyboardInterrupt:
         logging.warning("Process interrupted! Saving current state...")
-        save_last_seen_id(last_seen_id, last_seen_id_file)
-        save_plot(comments_processed, word_pairs_list)
+        #save_last_seen_id(last_seen_id, last_seen_id_file)
     except Exception as e:
         logging.error(f"Unexpected error: {e}")
     finally:
+        
         end_time = time.time()
         logging.info(f"Total processing time: {end_time - start_time:.2f} seconds.")
-process_batches(batch_size=100000, save_interval=10)
+
+process_batches(batch_size=100000, save_interval=20)
